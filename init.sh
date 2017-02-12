6c4ab3ce27d168e1359ec0d41dcf8cb68b422793
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-02-12 13:44:39 +0900

    update2

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 
-echo 'CREATE TABLE users(id integer primary key, username text unique, password text, handicap real);
-CREATE TABLE submits(id integer primary key, problem_id integer, user_id integer, score real, created_at real, input text);' | sqlite3 database.db
+echo 'CREATE TABLE IF NOT EXISTS users(id integer primary key, username text unique, password text, handicap real);
+CREATE TABLE IF NOT EXISTS submits(id integer primary key, problem_id integer, user_id integer, score real, created_at real, input text);' | sqlite3 database.db
 
 sudo chown `whoami`:www-data -R .
 sudo chmod 0770 -R .
+
+git update-index --skip-worktree settings.php
