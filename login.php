d4826c98ba6aa0d2f32b4747a45471f7a2fc8695
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-02-12 14:55:56 +0900

    update

--- login.php
+++ login.php
@@ -1,10 +1,18 @@
 <div>
-<h2>Login/Register</h2>
+<h2>Login</h2>
 <form action="#" method="post">
 <dl>
 	<dt>Username</dt><dd><input type="text" required name="username"></dd>
 	<dt>Password</dt><dd><input type="password" required name="password"></dd>
 	<dd><input type="submit" name="login" value="LOGIN"></dd>
+</dl>
+</form>
+
+<h2>Register</h2>
+<form action="#" method="post">
+<dl>
+	<dt>Username</dt><dd><input type="text" required name="username"></dd>
+	<dt>Password</dt><dd><input type="password" required name="password"></dd>
 	<dd><input type="submit" name="register" value="REGISTER"></dd>
 </dl>
 </form>
